9d437833e375594fb75b4117f03970af80c2b3c8
Matt Gibney <findmoppler@gmail.com>
2024-01-20 15:13:29 +0000

    Working ToDo List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [newToDo, setNewToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+
+  function addNewToDo() {
+    setToDos([
+      ...toDos,
+      {
+        text: newToDo,
+        isComplete: false,
+      }
+    ])
+    setNewToDo('');
+  }
+
+  function markDone(toDoIndex) {
+    const newToDos = [...toDos];
+    newToDos[toDoIndex].isComplete = !newToDos[toDoIndex].isComplete;
+
+    setToDos(newToDos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center h-full">
+      <div className="bg-slate-100 w-full max-w-3xl mx-auto p-10">
+        <div className='flex gap-x-4'>
+          <input placeholder='Enter Your ToDo' type="text" value={newToDo} onChange={(e) => setNewToDo(e.target.value)} className="border rounded w-full text-3xl p-4" />
+          <button className='text-5xl bg-white p-4 rounded-full' onClick={addNewToDo}>
+            &rarr;
+          </button>
+        </div>
+
+        <div className='bg-white mt-10'>
+          {toDos.map((toDo, index) => (
+            <button className='p-4 border-b flex w-full text-left' onClick={() => markDone(index)}>
+              <div className='w-full'>{toDo.text}</div>
+              <div className={`h-6 w-6 border border-gray-800 rounded ${toDo.isComplete ? 'bg-green-500' : 'bg-white'}`} />
+            </button>
+          ))}
+        </div>
+
+      </div>
     </div>
   );
 }
